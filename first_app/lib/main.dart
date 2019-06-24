a669f4ce1d4ddc6aff22803bd2f6c893c820e9df
Robert <robert.aydinyan2002@gmail.com>
2019-06-24 10:14:30 +0400

    buildproductlist

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,34 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/products_manager.dart';
-import 'package:first_app/products.dart';
+// import 'package:flutter/rendering.dart';
 
-main() {
+import './product_manager.dart';
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
-  build(context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-                appBar: AppBar(
-                  title: Text('first app'),
-                  backgroundColor: Colors.deepPurpleAccent,
-
-                ),
-                body:
-
-                    ProductsManager('Food Tester'),
-
-                ) );
+      // debugShowMaterialGrid: true,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager(),
+      ),
+    );
   }
 }
-
--- first_app/lib/products.dart
+++ first_app/lib/products.dart
@@ -1,35 +1,47 @@
 import 'package:flutter/material.dart';
 
-
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products(this.products);
+  Products([this.products = const []]) {
+    print('[Products Widget] Constructor');
+  }
 
-  Widget _buildProductItem(BuildContext context,int index){
+  Widget _buildProductList () {
+    print('[Products Widget] build()');
+    Widget productCard;
+    if (products.length > 0){
+      productCard = ListView.builder(
+          itemBuilder: _buildProductItem,
+          itemCount: products.length);
+
+    }else {
+      productCard = Center(child: Text('No products found'),);
+
+    }
+    return productCard;
+
+  }
+
+  Widget _buildProductItem(BuildContext context, int index) {
     return Card(
       child: Column(
         children: <Widget>[
           Image.asset('assets/food.jpg'),
-          Text(
-            products[index],
-            style: TextStyle(color: Colors.orange),
-          )
+          Text(products[index])
         ],
       ),
     );
-
-
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: _buildProductItem,
-      ),
-    );
+   return _buildProductList();
   }
 }
+
+
+
+
+
+
